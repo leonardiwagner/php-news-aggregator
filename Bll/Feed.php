8905a081a75fa84ddcc1af11c578f6a39d8935e2
Wagner Leonardi <leonardiwagner@gmail.com>
2013-10-29 17:18:58 -0200

    feed reader update

--- Bll/Feed.php
+++ Bll/Feed.php
@@ -7,47 +7,14 @@ include_once "../Model/FeedItem.php";
 
 class Feed{
 	
-	public function readFeed2($url){
-		$feed = file_get_contents($url);
-		$rss = new \SimpleXMLElement($feed);
-		
-		$rssFeed = new \Model\Feed();
-		$rssFeed->setTitle($rss->channel->title);
-		//$rssFeed->setDescription($rss->channel->description);
-		for($i=0; $i < sizeof($rss->channel->item); $i++)
-		{
-			$feedItem = new \Model\FeedItem();
-			$feedItem->setTitle((string)$rss->channel->item[$i]->title);
-			$feedItem->setLink((string)$rss->channel->item[$i]->link);
-			
-			$description = strip_tags((string)$rss->channel->item[$i]->description);
-			if(strlen($description) > 250)
-			{
-				$description = substr($description, 0, 250) . "...";
-			}
-			$feedItem->setDescription($description);
-			
-			$date = strtotime((string)$rss->channel->item[$i]->pubDate);
-			$feedItem->setPublicationDate(date('Y-m-d H:i:s', $date));
-			
-			$rssFeed->addFeedItem($feedItem);
-		}
-		
-		$rssFeed->setUrl($url);
-		
-		return $rssFeed;
-	}
-	
+	//Read RSS 1.0
 	public function readFeed1($url){
 		$feed = file_get_contents($url);
 		$rss = new \SimpleXMLElement($feed);
-		
-	
+
 		$rssFeed = new \Model\Feed();
 		$rssFeed->setTitle($rss->title);
 		
-				
-
 		for($i=0; $i < sizeof($rss->entry); $i++)
 		{
 			$feedItem = new \Model\FeedItem();
@@ -71,5 +38,38 @@ class Feed{
 		
 		return $rssFeed;
 	}
-}
+	
+	//Read RSS 2.0
+	public function readFeed2($url){
+		$feed = file_get_contents($url);
+		$rss = new \SimpleXMLElement($feed);
+		
+		$rssFeed = new \Model\Feed();
+		$rssFeed->setTitle($rss->channel->title);
+		for($i=0; $i < sizeof($rss->channel->item); $i++)
+		{
+			$feedItem = new \Model\FeedItem();
+			$feedItem->setTitle((string)$rss->channel->item[$i]->title);
+			$feedItem->setLink((string)$rss->channel->item[$i]->link);
+			
+			$description = strip_tags((string)$rss->channel->item[$i]->description);
+			if(strlen($description) > 250)
+			{
+				$description = substr($description, 0, 250) . "...";
+			}
+			$feedItem->setDescription($description);
+			
+			$date = strtotime((string)$rss->channel->item[$i]->pubDate);
+			$feedItem->setPublicationDate(date('Y-m-d H:i:s', $date));
+			
+			$rssFeed->addFeedItem($feedItem);
+		}
+		
+		$rssFeed->setUrl($url);
+		
+		return $rssFeed;
+	}
+
+	
+}// end class
 ?>
